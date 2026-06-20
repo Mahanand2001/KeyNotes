5ffa421ebdf7d2676aca5e777e90817dd0b97149
Mahanand Omkar Sharma <Sharmamahanand387@gmail.com>
2026-06-20 23:44:48 +0530

    Second commit
    Configured tailwind CSS into the project and fixed routes for note create and note detail page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,14 @@
-import React from 'react'
+
 import { Route, Routes } from 'react-router';
 import HomePage from './pages/HomePage';
 import Create from './pages/create';
 import NoteDetailPage from './pages/NoteDetailPage';
+import {toast} from 'react-hot-toast'
 
 const  App = () => {
   return (
     <div>
+      <button onClick={() => toast.success("fuck you")} className='text-red-500 p-4 bg-pink-300'>click me</button>
       <Routes>
         <Route path='/' element={<HomePage />} />
         <Route path='/create' element={<Create />} />
