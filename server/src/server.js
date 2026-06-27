219ee7c5f8516299a485b5bdd928ebb8ac911fa4
Mahanand Omkar Sharma <Sharmamahanand387@gmail.com>
2026-06-27 23:50:51 +0530

    Deployment ready

--- server/src/server.js
+++ server/src/server.js
@@ -6,9 +6,12 @@ import connectDB from "./config/db.js";
 import dotenv from "dotenv";
 import rateLimiter from './middleware/rateLimiter.js'
 import cors from 'cors'
+import path from 'path'
 
 
 dotenv.config();
+
+const __dirname = path.resolve()
 
 
 const app = express();
@@ -17,17 +20,26 @@ const app = express();
 //middleware
 app.use(express.json());
 
-app.use(cors({
+if(process.env.NODE_ENV !== "production"){
+    app.use(cors({
     origin: "http://localhost:5173",
-}));
+    }));
+}
 
 app.use(rateLimiter);
 
 
-
-
 app.use('/api/notes', notesRoutes)
 
+
+if(process.env.NODE_ENV === "production"){
+
+    app.use(express.static(path.join(__dirname, "../client/dist")))
+
+    app.get("*splat", (req, res) => {
+        res.sendFile(path.join(__dirname, "../client/", "dist", "index.html"))
+    })
+}
 
 connectDB().then(() => {app.listen(5001, () => {
     console.log("server is running on port 5001");
